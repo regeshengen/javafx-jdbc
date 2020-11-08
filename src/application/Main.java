4f064efec4528622399318ddb6c15b74a3e38bb8
regeshengen <reges@unochapeco.edu.br>
2020-11-08 20:06:51 -0300

    View Sobre - carregamento

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,22 +9,29 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			
+
 			scrollPane.setFitToHeight(true); // seta o menu no topo
 			scrollPane.setFitToWidth(true); // seta largura fixa ao menu
-			
-			Scene mainScene = new Scene(scrollPane);
+
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Hellow World");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
